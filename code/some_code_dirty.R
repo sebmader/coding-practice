52b53f3f9a048633c8ab271b5e78d783878e9665
smad <ea10akaz@uni-leipzig.de>
2025-10-10 19:01:36 +0200

    made code even more ugly by removing more spaces

--- code/some_code_dirty.R
+++ code/some_code_dirty.R
@@ -15,29 +15,29 @@ View(palmerpenguins::penguins)
 # Load and clean the data: remove rows with NA values         
 df<-na.omit(palmerpenguins::penguins)
     # calculate flipper-body mass ratio as a proxy for swim speed
-    df <- df%>%mutate(fl.bm_ratio=flipper_length_mm/(body_mass_g/1000))
+    df<-df%>%mutate(fl.bm_ratio=flipper_length_mm/(body_mass_g/1000))
 View(df)
 # Restructure the dataset: select relevant columns
-df.2 <- df %>% select(!(bill_length_mm:bill_depth_mm))
+df.2<-df %>% select(!(bill_length_mm:bill_depth_mm))
 View(df.2)
 # First plot: Scatterplot with ggplot2
-ggplot(data= df.2, aes(x=species, y=fl.bm_ratio, color= sex)) +
+ggplot(data= df.2, aes(x=species,y=fl.bm_ratio,color= sex)) +
 geom_boxplot() +
-stat_summary(fun.data =mean_se,geom = "errorbar", colour= "darkgrey", width =0.1,size=0.8) +
-stat_summary(fun=mean,geom ="point",shape=23,fill ="red",colour ="darkgrey", size =2) +
-labs(title="Flipper length-bodymass ratio of different species",x="Species",y ="Flipper-body mass ratio [mm/kg]") +
+stat_summary(fun.data =mean_se,geom="errorbar",colour= "darkgrey", width=0.1,size=0.8)+
+stat_summary(fun=mean,geom ="point",shape=23,fill ="red",colour="darkgrey", size =2) +
+labs(title="Flipper length-bodymass ratio of different species",x="Species",y="Flipper-body mass ratio [mm/kg]")+
   theme_bw()
 # Simple statistical analysis: Linear model
-model <- lm(fl.bm_ratio~species * sex,data =df.2)
+model<-lm(fl.bm_ratio~species *sex,data=df.2)
   anova(model)
 # Add model predictions to the original data
-df.2$predicted_fl.bm_ratio<-predict(model, newdata = df.2)
+df.2$predicted_fl.bm_ratio<-predict(model,newdata=df.2)
 # Second plot: Adding prediction
-ggplot(data= df.2, aes(x =species, y =fl.bm_ratio, color = sex)) +
+ggplot(data= df.2,aes(x=species,y=fl.bm_ratio,color = sex)) +
   geom_boxplot() +
-labs(title="Flipper length-bodymass ratio of different species", x= "Species", y ="Flipper-body mass ratio [mm/kg]") +
+labs(title="Flipper length-bodymass ratio of different species",x="Species",y="Flipper-body mass ratio [mm/kg]")+
   geom_point(aes(y =predicted_fl.bm_ratio),shape = 23, fill = "red", size =2) +
-labs(title = "Predicted flipper length-bodymass ratios", x ="Species", y= "Flipper-body mass ratio [mm/kg]") +
+labs(title="Predicted flipper length-bodymass ratios",x="Species",y="Flipper-body mass ratio [mm/kg]") +
   theme_bw()
 
 
